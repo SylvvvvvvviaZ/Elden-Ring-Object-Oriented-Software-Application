586e126522b3e4cac5c7dabb6bd48e6bce91b84e
dkon0020 <dkon0020@student.monash.edu>
2023-04-01 15:27:29 +1100

    Implement Req 1.c.

---
+++ src/game/Grossmesser.java
@@ -0,0 +1,9 @@
+package game;
+
+import edu.monash.fit2099.engine.weapons.WeaponItem;
+
+public class Grossmesser extends WeaponItem {
+    public Grossmesser() {
+        super("Grossmesser", '?', 115, "hit", 85);
+    }
+}
