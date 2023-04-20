33b827a2c31d5433165e455069f9203920324e22
dkon0020 <dkon0020@student.monash.edu>
2023-04-20 15:43:14 +1000

    Start abstract factory for east-west enemies

---
+++ src/game/factories/WestEnemyFactory.java
@@ -0,0 +1,21 @@
+package game.factories;
+
+import edu.monash.fit2099.engine.positions.GameMap;
+import game.interfaces.EnemyFactory;
+
+public class WestEnemyFactory implements EnemyFactory {
+    @Override
+    public void generateSkeletal(GameMap gameMap) {
+
+    }
+
+    @Override
+    public void generateCanine(GameMap gameMap) {
+
+    }
+
+    @Override
+    public void generateCrustacean(GameMap gameMap) {
+
+    }
+}
---
+++ src/game/interfaces/EnemyFactory.java
@@ -0,0 +1,26 @@
+package game.interfaces;
+
+import edu.monash.fit2099.engine.positions.GameMap;
+
+/**
+ * Interface for generating enemies on different maps
+ */
+public interface EnemyFactory {
+    /**
+     * Generate Skeletal-type enemies
+     * @param gameMap the game map to generate the enemies
+     */
+    void generateSkeletal(GameMap gameMap);
+
+    /**
+     * Generate Canine-type enemies
+     * @param gameMap the game map to generate the enemies
+     */
+    void generateCanine(GameMap gameMap);
+
+    /**
+     * Generate Crustacean-type enemies
+     * @param gameMap the game map to generate the enemies
+     */
+    void generateCrustacean(GameMap gameMap);
+}
