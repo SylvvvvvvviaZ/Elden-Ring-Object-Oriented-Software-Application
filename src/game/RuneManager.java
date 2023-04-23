a7797bf48d05c29c484fcfeacdf6e56d70bc3120
dkon0020 <dkon0020@student.monash.edu>
2023-04-23 13:48:22 +1000

    Implement dropping of runes on player death
    Contribution: everyone

--- src/game/RuneManager.java
+++ src/game/RuneManager.java
@@ -2,37 +2,39 @@ package game;
 
 import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.positions.GameMap;
+import edu.monash.fit2099.engine.positions.Location;
 import game.currency.CurrencyItem;
+import game.currency.Rune;
 
 import java.util.HashMap;
 
 /**
  * Singleton for handling entities in the game holding currency/currencies
  */
-public class CurrencyManager implements Resettable {
+public class RuneManager implements Resettable {
     /**
      * Instance of the Currency Manager
      */
-    private static CurrencyManager instance;
+    private static RuneManager instance;
     /**
      * Record of actors' currency holdings
      */
-    private final HashMap<Actor, HashMap<Class<? extends CurrencyItem>, Integer>> currencyRecord;
+    private final HashMap<Actor, Integer> currencyRecord;
 
     /**
      * Gets an instance of Currency Manager
      *
      * @return instance of Currency Manager
      */
-    public static CurrencyManager getInstance() {
-        if (instance == null) instance = new CurrencyManager();
+    public static RuneManager getInstance() {
+        if (instance == null) instance = new RuneManager();
         return instance;
     }
 
     /**
      * Constructor
      */
-    private CurrencyManager() {
+    private RuneManager() {
         currencyRecord = new HashMap<>();
     }
 
@@ -44,7 +46,7 @@ public class CurrencyManager implements Resettable {
      * @return true if the actor has enough of that currency in their balance; false otherwise
      */
     public boolean hasSufficientBalance(Actor actor, CurrencyItem currencyItem) {
-        return currencyRecord.get(actor).get(currencyItem.getClass()) >= currencyItem.getValue();
+        return currencyRecord.get(actor) >= currencyItem.getValue();
     }
 
     /**
@@ -54,16 +56,7 @@ public class CurrencyManager implements Resettable {
      * @param currencyItem the currency item to add
      */
     public void addMoney(Actor actor, CurrencyItem currencyItem) {
-        if (currencyRecord.get(actor).containsKey(currencyItem.getClass())) {
-            // If actor has this currency item, update the balance
-            currencyRecord.get(actor).put(
-                    currencyItem.getClass(),
-                    currencyRecord.get(actor).get(currencyItem.getClass()) + currencyItem.getValue()
-            );
-        } else {
-            // If the actor does not have this currency item, make a new entry for it
-            currencyRecord.get(actor).put(currencyItem.getClass(), currencyItem.getValue());
-        }
+        currencyRecord.put(actor, currencyItem.getValue());
     }
 
     /**
@@ -76,28 +69,28 @@ public class CurrencyManager implements Resettable {
      * @return true if the actor's balance has been updated, false if the actor does not hold this currency item or balance is insufficient
      */
     public boolean removeMoney(Actor actor, CurrencyItem currencyItem) {
-        if (currencyRecord.get(actor).containsKey(currencyItem.getClass())) {
-            if (currencyRecord.get(actor).get(currencyItem.getClass()) < currencyItem.getValue()) return false;
-            currencyRecord.get(actor).put(
-                    currencyItem.getClass(),
-                    currencyRecord.get(actor).get(currencyItem.getClass()) - currencyItem.getValue()
-            );
+            if (currencyRecord.get(actor) < currencyItem.getValue()) return false;
+            currencyRecord.put(actor, currencyRecord.get(actor) - currencyItem.getValue());
             return true;
-        }
-        return false;
+    }
+
+    public void resetActor(Actor actor, GameMap gameMap, Location lastLocation) {
+        CurrencyItem runes = new Rune(currencyRecord.get(actor));
+        currencyRecord.put(actor, 0);
+        ResetManager.getInstance().registerResettable(runes);
+        lastLocation.addItem(runes);
     }
 
     @Override
     public void reset(ResetType resetType, GameMap gameMap) {
         if (resetType == ResetType.RESET_ON_DEATH) {
             // Player has died
-            currencyRecord.forEach( ((actor, currencies) -> {
+            currencyRecord.forEach( ((actor, quantity) -> {
                 if (actor.hasCapability(Status.HOSTILE_TO_ENEMY)) {
                     // Their monies should be reset
                     // Drop all their currencies
-                    currencies.forEach( ((currency, amount) -> {
-
-                    }));
+                    // TODO: Drop runes in last-turn location
+                    new Rune(quantity).getDropAction(actor).execute(actor, gameMap);
                 }
             }));
         }
