1329418eb4c10687dfa8a4608a084f84f56951b8
xzho0118 <xzho0118@student.monash.edu>
2023-05-21 09:25:24 +1000

    add new ground cliff

--- src/game/grounds/Cliff.java
+++ src/game/grounds/Cliff.java
@@ -1,4 +1,15 @@
 package game.grounds;
 
-public class Cliff {
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.positions.Ground;
+import game.Status;
+
+public class Cliff extends Ground {
+    public Cliff() {super('+');
+    }
+
+    @Override
+    public boolean canActorEnter(Actor actor) {
+        return false;
+    }
 }
